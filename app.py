aca88c3ea2a4d36e886a903d518bf863bc4246ec
Guilherme Juventino <guilhermejuventino@gmail.com>
2023-06-25 12:30:32 -0300

    Converted portfolio page into a Flask project

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/contacts")
+def contacts():
+    return render_template("contacts.html")
+
+
+@app.route("/languages")
+def languages():
+    return render_template("languages.html")
+
+
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+
+
+if __name__ == "__main__":
+    app.run()
